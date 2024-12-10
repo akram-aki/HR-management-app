8f511046734251a926bbe39ff8938d6b9348a4e2
akram <son.gogo@hotmail.com>
2024-12-10 08:32:26 +0100

    added routes, added absence page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./components/homePage/Index";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/NavBar/Layout";
 import axios from "axios";
+import AbsencePage from "./components/absencePage/Index";
 
 axios.defaults.baseURL = "http://localhost:8000/";
 
@@ -16,6 +17,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Layout />}>
               <Route path="/homePage" element={<HomePage />} />
+              <Route path="/absence" element={<AbsencePage />} />
             </Route>
           </Routes>
         </Router>
